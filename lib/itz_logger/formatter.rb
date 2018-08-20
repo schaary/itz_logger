2f7afe031149801aa7cdf05721064f4f133cbd3e
Michael Schaarschmidt <michael.schaarschmidt@itz.uni-halle.de>
2018-08-20 18:25:13 +0200

    change log format

--- lib/itz_logger/formatter.rb
+++ lib/itz_logger/formatter.rb
@@ -10,7 +10,7 @@ module ItzLogger
 
     def self.format(message)
       printf(
-        "%-20s %-#{message.type.length}s %s\n",
+        "%-19s %-#{message.type.length}s %s\n",
         message.created_at.strftime("%b %d %H:%M:%S UTC"),
         message.type.to_s.upcase,
         message.message)
