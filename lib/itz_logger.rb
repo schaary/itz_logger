a581021db4aa9fa3b176f3ecd199c77d726bae17
Michael Schaarschmidt <michael.schaarschmidt@itz.uni-halle.de>
2018-08-20 16:17:17 +0200

    fix typo

--- lib/itz_logger.rb
+++ lib/itz_logger.rb
@@ -3,7 +3,7 @@ require "itz_logger/version"
 require "itz_logger/logger"
 require "itz_logger/message"
 require "itz_logger/message_type"
-require "itz_logger/log_stategy"
+require "itz_logger/log_strategy"
 require "itz_logger/log_terminal"
 
 module ItzLogger
