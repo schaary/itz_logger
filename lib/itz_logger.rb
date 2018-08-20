94d27cf81ff987f85454fd7be7469f36c94f0bd3
Michael Schaarschmidt <michael.schaarschmidt@itz.uni-halle.de>
2018-08-20 16:15:15 +0200

    add reguire statements

--- lib/itz_logger.rb
+++ lib/itz_logger.rb
@@ -1,5 +1,11 @@
 require "itz_logger/version"
 
+require "itz_logger/logger"
+require "itz_logger/message"
+require "itz_logger/message_type"
+require "itz_logger/log_stategy"
+require "itz_logger/log_terminal"
+
 module ItzLogger
   # Your code goes here...
 end
